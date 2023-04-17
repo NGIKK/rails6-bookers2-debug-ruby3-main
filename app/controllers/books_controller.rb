1bb72871eb1eeb97052260ea27b8872c6f9c2008
NGIKK <129597259+NGIKK@users.noreply.github.com>
2023-04-17 05:30:29 +0000

    [Finish]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,65 @@
+class BooksController < ApplicationController
+   before_action :ensure_correct_user, only: [:edit,:update]
+
+  def show
+    @user = current_user
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def create
+    @user = current_user
+    @books = Book.all
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      #@books = Book.all
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    # unless @book.user == current_user
+      # redirect_to books_path
+    # end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+# メソッドを定義してbefore_actionする
+   def ensure_correct_user
+     @book = Book.find(params[:id])
+     unless @book.user == current_user
+       redirect_to books_path
+     end
+   end
+   
+end
