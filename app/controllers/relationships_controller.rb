179a87eec7f4b82c7e3a935e1f089c51a57ca574
NGIKK <129597259+NGIKK@users.noreply.github.com>
2023-04-21 05:21:47 +0000

    [Fix]フォロー/フォロワー機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,25 +4,32 @@ class RelationshipsController < ApplicationController
     #user = User.find(params[:user_id])
     #relationship = current_user.relationships.new(followed_id: user.id)
     #relationship.save
-    current_user.follow(params[:user_id])
+    user = User.find(params[:user_id])
+    current_user.follow(user)
+    #current_user.follow(params[:user_id])
+    #上記は自分の記述。params[:user_id]の（小文字のモデル名）_idは
+    #そのモデルのidということを明示的に表している。
     redirect_to request.referer
   end
 
   def destroy
-    current_user.unfollow(params[:user_id])
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)
+    #current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
-  
+
   def followings
     user = User.find(params[:user_id])
     @followings = user.followings
     #なぜfollowed_idじゃない？
   end
-  
+
   def followers
     user  = User.find(params[:user_id])
     @followers = user.followers
     #なぜfollower_idじゃない？？
   end
-  
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,21 @@ class User < ApplicationRecord
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
  
+ 
+ 
+  #受動的関係。フォローされる
+   # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship",foreign_key: "followed_id", dependent: :destroy
+   # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+ 
  #能動的関係。フォローする
+   # 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship",foreign_key: "follower_id", dependent: :destroy
+   # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed
  
- #受動的関係。フォローされる
-  has_many :reverse_of_relationships, class_name: "Relationship",foreign_key: "followed_id", dependent: :destroy
-  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   
   has_one_attached :profile_image
 
@@ -29,12 +37,12 @@ class User < ApplicationRecord
   end
   
   
-  def follow(user_id)
-      relationships.create(followed_id: user_id)
+  def follow(user)
+      relationships.create(followed_id: user.id)
   end
       
-  def unfollow(user_id)
-      relationships.find_by(followed_id: user_id).destroy
+  def unfollow(user)
+      relationships.find_by(followed_id: user.id).destroy
   end
   
   def following?(user)
