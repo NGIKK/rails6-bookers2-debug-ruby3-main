ab25e711ee987f3c6833d29332b6b670300eeeab
NGIKK <129597259+NGIKK@users.noreply.github.com>
2023-04-23 08:38:08 +0000

    [Add]検索機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,13 +2,29 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
       favorites.exists?(user_id: user.id)
       #なぜ(user.id)だけじゃいけないのか？？user_idカラムのuser.idを指定している？
   end
-  
+
+ def self.looks(searches,words)
+   if searches == "perfect_match"
+      @book = Book.where("title LIKE ?","#{words}")
+      #完全一致
+   elsif searches == "forward_match"
+      @book = Book.where("title LIKE ?","#{words}%")
+      #前方一致
+   elsif searches == "backward_match"
+      @book = Book.where("title LIKE ?","%#{words}")
+      #後方一致
+   else
+      @book = Book.where("title LIKE ?","%#{words}%")
+      #部分一致。%は任意の文字列を表す。複数形であることに処理的な意味はない。
+   end
+ end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,46 +7,62 @@ class User < ApplicationRecord
   has_many :books,dependent: :destroy
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
- 
- 
- 
+
+
+
   #受動的関係。フォローされる
    # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship",foreign_key: "followed_id", dependent: :destroy
    # 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_of_relationships, source: :follower
- 
+
  #能動的関係。フォローする
    # 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship",foreign_key: "follower_id", dependent: :destroy
    # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed
- 
 
-  
+
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   # presence: true はミニマムが２で指定されているので不要？
   validates :introduction,length: {maximum: 50 }
 
-  
-  
+
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
-  
+
+
   def follow(user)
       relationships.create(followed_id: user.id)
   end
-      
+
   def unfollow(user)
       relationships.find_by(followed_id: user.id).destroy
   end
-  
+
   def following?(user)
       followings.include?(user)
   end
-  
+
+  def self.looks(searches,words)
+   if searches == "perfect_match"
+      @user = User.where("name LIKE ?","#{words}")
+      #完全一致
+   elsif searches == "forward_match"
+      @user = User.where("name LIKE ?","#{words}%")
+      #前方一致
+   elsif searches == "backward_match"
+      @user = User.where("name LIKE ?","%#{words}")
+      #後方一致
+   else
+      @user = User.where("name LIKE ?","%#{words}%")
+      #部分一致。%は任意の文字列を表す。複数形であることに処理的な意味はない。
+   end
+  end
+
 end
