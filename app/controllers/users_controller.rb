2957e6ad20b234ea511b4ae57eb434a2b37f2965
NGIKK <129597259+NGIKK@users.noreply.github.com>
2023-05-08 12:45:58 +0000

    [Add]投稿数グラフ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @yesterday_book = @books.created_yesterday
     @this_week_book = @books.created_this_week
     @last_week_book = @books.created_last_week
+    @date_book = @books.date_looks(params[:created_at])
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -15,6 +15,10 @@ class Book < ApplicationRecord
   scope :created_this_week, ->{where(created_at: 6.day.ago.beginning_of_day .. Time.zone.now.end_of_day)}
   scope :created_last_week, ->{where(created_at: 2.week.ago.beginning_of_day .. 1.week.ago.end_of_day)}
 
+  def self.this_week_count
+   (0..6).map { |n| created_days_ago(n).count }.reverse
+  end
+
   def favorited_by?(user)
       favorites.exists?(user_id: user.id)
       #user_idカラムからviewの引数で渡してくるuser.idが存在しているか確認している。
@@ -36,6 +40,11 @@ class Book < ApplicationRecord
       # Book.where('title LIKE ?', '%'+word+'%')でも
    end
  end
+ 
+ def self.date_looks(date)
+   @book = Book.where(created_at:)
+     
+ end 
 
 
 end
